fc9d224ca0c62e2a2acb569ac676c3c1a58d242d
lulzshadowwalker <lulzshadowwalker@gmail.com>
2025-08-03 16:13:26 +0300

    Refactor API routes by removing unused user route and cleaning up formatting

--- routes/api.php
+++ routes/api.php
@@ -4,49 +4,16 @@ use App\Http\Controllers\Api\OrganizationController;
 use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\SkillController;
 use App\Http\Controllers\Api\LocationController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\VolunteeringInterestController;
 
-Route::get("/user", function (Request $request) {
-    return $request->user();
-})->middleware("auth:sanctum");
+Route::get("/organizations", [OrganizationController::class, "index"])->name("api.organizations.index");
+Route::get("/organizations/{organization:slug}", [OrganizationController::class, "show",])->name("api.organizations.show");
 
-Route::get("/organizations", [OrganizationController::class, "index"])->name(
-    "api.organizations.index"
-);
+Route::get("/skills", [SkillController::class, "index"])->name("api.skills.index");
+Route::get("/skills/{skill}", [SkillController::class, "show"])->name("api.skills.show");
 
-Route::get("/organizations/{organization:slug}", [
-    OrganizationController::class,
-    "show",
-])->name("api.organizations.show");
-
-Route::get("/skills", [SkillController::class, "index"])->name(
-    "api.skills.index"
-);
-
-Route::get("/skills/{skill}", [SkillController::class, "show"])->name(
-    "api.skills.show"
-);
-
-Route::get("/locations", [LocationController::class, "index"])->name(
-    "api.locations.index"
-);
-
-Route::get("/locations/{location}", [LocationController::class, "show"])->name(
-    "api.locations.show"
-);
-
-Route::get("/skills", [SkillController::class, "index"])->name(
-    "api.skills.index"
-);
-
-Route::get("/skills/{skill}", [SkillController::class, "show"])->name(
-    "api.skills.show"
-);
+Route::get("/locations", [LocationController::class, "index"])->name("api.locations.index");
+Route::get("/locations/{location}", [LocationController::class, "show"])->name("api.locations.show");
 
 Route::get("/posts", [PostController::class, "index"])->name("api.posts.index");
-
-Route::get("/posts/{post:slug}", [PostController::class, "show"])->name(
-    "api.posts.show"
-);
+Route::get("/posts/{post:slug}", [PostController::class, "show"])->name("api.posts.show");
