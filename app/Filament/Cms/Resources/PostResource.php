d781452e19b1e1f4d250bda6e772089d2732fcf0
lulzshadowwalker <lulzshadowwalker@gmail.com>
2025-09-12 11:00:57 +0300

    feat: add PostResource with CRUD pages for managing posts

---
+++ app/Filament/Cms/Resources/PostResource.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Filament\Cms\Resources;
+
+use App\Filament\Cms\Resources\PostResource\Pages;
+use App\Filament\Cms\Resources\PostResource\RelationManagers;
+use App\Models\Post;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PostResource extends Resource
+{
+    protected static ?string $model = Post::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->required(),
+                Forms\Components\MarkdownEditor::make('content')
+                    ->required()
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable(),
+                // Tables\Columns\TextColumn::make('views_count')
+                //     ->getStateUsing(fn(Post $record): int => 12)
+                //     ->alignRight()
+                //     ->badge()
+                //     ->icon('heroicon-o-eye')
+                //     ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label('Published At')
+                    ->description(fn(Post $record): string => $record->created_at?->diffForHumans() ?? '')
+                    ->alignRight()
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPosts::route('/'),
+            'create' => Pages\CreatePost::route('/create'),
+            'edit' => Pages\EditPost::route('/{record}/edit'),
+        ];
+    }
+}
