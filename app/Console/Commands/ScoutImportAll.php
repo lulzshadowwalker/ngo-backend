bc4736454cd42c31aeb66eecbb89a1b0999d0ba3
lulzshadowwalker <lulzshadowwalker@gmail.com>
2025-09-13 10:58:05 +0300

    feat: Add ScoutImportAll command to import all models configured for Laravel Scout; update OpportunityFactory to return arrays for tags

---
+++ app/Console/Commands/ScoutImportAll.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Laravel\Scout\EngineManager;
+
+class ScoutImportAll extends Command
+{
+    protected $signature = 'scout:import-all';
+    protected $description = 'Import all models configured for Laravel Scout';
+
+    public function handle()
+    {
+        $models = config('scout.typesense.model-settings', []);
+        foreach ($models as $class => $schema) {
+            $this->call('scout:import', ['model' => $class]);
+        }
+        $this->info('All Scout models imported!');
+    }
+}
