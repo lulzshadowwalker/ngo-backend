8cd31fd8ba9bfac070a15a977c9b2b9dc9d0b9cc
lulzshadowwalker <lulzshadowwalker@gmail.com>
2025-09-14 08:45:49 +0300

    fix: Update parameter key for scout:index command in ScoutImportAll

--- app/Console/Commands/ScoutImportAll.php
+++ app/Console/Commands/ScoutImportAll.php
@@ -3,7 +3,6 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use Laravel\Scout\EngineManager;
 
 class ScoutImportAll extends Command
 {
@@ -14,7 +13,7 @@ class ScoutImportAll extends Command
     {
         $models = config('scout.typesense.model-settings', []);
         foreach ($models as $class => $schema) {
-            $this->call('scout:index', [$class]);
+            $this->call('scout:index', ['name' => $class]);
             $this->call('scout:import', ['model' => $class]);
         }
         $this->info('All Scout models imported!');
